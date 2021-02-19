5b03d0ef0025fe207a5d9fad7ffc6a54c3f6fe37
manasmadrecha <madrechamanas@gmail.com>
2021-02-19 22:19:39 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  prefix: 'tw-',
+}
