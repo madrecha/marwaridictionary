3bb0026ed0052d2212d6463208ca1473bbc6fc12
manasmadrecha <madrechamanas@gmail.com>
2021-02-20 03:48:18 +0530

    added about page

---
+++ content/about.md
@@ -0,0 +1,9 @@
+---
+title: About
+slugurl: about
+description: 
+---
+
+A Place to learn Marwari / Marwadi and improve your vocabulary of Marwari Words. 
+
+If you already know better, then join us to help others learn.
---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1
+      class="tw-text-3xl tw-text-center tw-bg-blue-50 tw-text-pink-900 tw-p-3"
+    >
+      About Marwari Dictionary
+    </h1>
+    <NuxtContent :document="page"></NuxtContent>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      page: null,
+    };
+  },
+  async fetch() {
+    this.page = await this.$content("about").fetch();
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
