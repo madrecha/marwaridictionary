c23b8e2909ecc13eca08032fe3ae0e9767620fb1
manasmadrecha <madrechamanas@gmail.com>
2021-02-19 22:19:09 +0530

    all words index page

---
+++ pages/word/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    main word file ka index hoon
+    <div v-for="word in words" :key="word.slugurl">
+      <nuxt-link :to="`/word/${word.slugurl}`">
+        {{ word.title }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      words: [],
+    };
+  },
+  async fetch() {
+    this.words = await this.$content("words")
+      //   .where({ slugurl: this.$route.params.slugurl })
+      .fetch();
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
