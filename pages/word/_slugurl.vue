719209792074937c795364c1c9095598e8bcf2cc
manasmadrecha <madrechamanas@gmail.com>
2021-02-20 02:03:40 +0530

    styling, removed old comments

--- pages/word/_slugurl.vue
+++ pages/word/_slugurl.vue
@@ -1,26 +1,25 @@
 <template>
   <div>
     <div v-for="word in words" :key="word.slugurl">
-      <h1>Marwari word: {{ word.title }}</h1>
+      <h1 class="tw-text-3xl tw-text-pink-800 tw-text-center tw-font-medium">
+        Marwari word: {{ word.title }}
+      </h1>
       <NuxtContent :document="word"></NuxtContent>
       <div v-if="word.conjugation">
-        <h2>Conjugation</h2>
-        <MarwariVerbConjugation
-          :word="word.conjugation"
-          :worden="word.conjugationen"
-        ></MarwariVerbConjugation>
-      </div>
-      <!-- <h2>Meaning</h2> -->
-      <!-- <h3>English meaning:</h3>
-      {{ word.meaning.en }}
-      <h3>Hindi meaning</h3>
-      {{ word.meaning.hi }}
-      <h2>Example</h2>
-      {{ word.examples }} -->
-      <div v-for="example in word.examples" :key="example">
-        <!-- <p>{{ word.examples }}</p> -->
-        <!-- <p>English meaning: {{ example.en }}</p> -->
-        <!-- <p>Hindi meaning: {{ example.hi }}</p> -->
+        <h2
+          class="tw-text-pink-800 tw-text-xl sm:tw-text-3xl tw-mt-4 tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 lg:tw--ml-2 tw-pl-3 tw-shadow-md"
+        >
+          Conjugation
+        </h2>
+        <div
+          class="tw-mt-3 tw-max-w-xs tw-overflow-x-scroll sm:tw-overflow-x-auto sm:tw-max-w-full"
+        >
+          <MarwariVerbConjugation
+            :word="word.conjugation.term"
+            :worden="word.conjugation.en"
+            :wordtype="word.conjugation.type"
+          ></MarwariVerbConjugation>
+        </div>
       </div>
     </div>
   </div>
