32168e50a3a5ee729c552d501d71e3e360acafd0
manasmadrecha <madrechamanas@gmail.com>
2021-02-19 07:32:05 +0530

    basic header component added for initial layout

---
+++ components/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tw-flex tw-bg-pink-100">
+    <div class="tw-mx-auto">
+      <nuxt-link to="/">Home</nuxt-link>
+      <nuxt-link to="/word">All Words</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
