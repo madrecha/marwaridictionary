5a519455e0b468cf3050053208f8ffafc4f26efd
manasmadrecha <madrechamanas@gmail.com>
2021-02-20 02:51:38 +0530

    basic css 4 initial good layout; no. of words div

--- pages/word/index.vue
+++ pages/word/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <div>
-    main word file ka index hoon
-    <div v-for="word in words" :key="word.slugurl">
-      <nuxt-link :to="`/word/${word.slugurl}`">
-        {{ word.title }}
-      </nuxt-link>
+  <div class="tw-mt-3">
+    <div class="tw-text-center tw-text-2xl">
+      {{ words.length }} words added till now
+    </div>
+    <div class="tw-mt-3">
+      <ul class="tw-flex tw-flex-wrap">
+        <li
+          v-for="word in words"
+          :key="word.slugurl"
+          class="tw-p-3 hover:tw-bg-blue-50 tw-text-lg tw-text-center"
+        >
+          <nuxt-link :to="`/word/${word.slugurl}`">
+            {{ word.title }}<br /><span class="tw-text-sm">
+              {{ word.transliteration }}
+            </span>
+          </nuxt-link>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
