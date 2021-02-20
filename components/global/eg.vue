636165a72feef89fd2d2ab99ee628a4e28365230
manasmadrecha <madrechamanas@gmail.com>
2021-02-21 02:36:44 +0530

    Example component with lang

---
+++ components/global/eg.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="eacheg in eg" :key="eacheg.eg">
+      {{ eacheg.eg }}
+      <ul>
+        <li v-if="eacheg.en">
+          <span class="tw-text-xs">English:</span> {{ eacheg.en }}
+        </li>
+        <li v-if="eacheg.hi">
+          <span class="tw-text-xs">Hindi:</span> {{ eacheg.hi }}
+        </li>
+        <li v-if="eacheg.mr">
+          <span class="tw-text-xs">Marathi:</span> {{ eacheg.mr }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: { eg: Array },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
