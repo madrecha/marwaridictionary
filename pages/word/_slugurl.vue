d2300739e591be93404a06b68eb7051feb15ed80
manasmadrecha <madrechamanas@gmail.com>
2021-02-21 01:52:52 +0530

    grammar v-if added at bottom

--- pages/word/_slugurl.vue
+++ pages/word/_slugurl.vue
@@ -5,6 +5,39 @@
         {{ word.title }} ({{ word.transliteration }})
       </h1>
       <NuxtContent :document="word"></NuxtContent>
+      <div v-if="word.grammar" class="tw-mt-8">
+        <h2
+          class="tw-text-pink-800 tw-text-xl sm:tw-text-3xl tw-mt-4 tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 lg:tw--ml-2 tw-pl-3 tw-shadow-md"
+        >
+          Grammar
+        </h2>
+        <div v-if="word.grammar.noun">
+          Noun,
+          {{
+            word.grammar.noun.gender === "m"
+              ? "Masculine Noun"
+              : "Feminine Noun"
+          }},
+          {{
+            word.grammar.noun.number === "s" ? "Singular Noun" : "Plural Noun"
+          }}
+        </div>
+        <div v-if="word.grammar.adjective">
+          Adjective,
+          {{
+            word.grammar.adjective.gender === "m"
+              ? "Masculine adjective"
+              : "Feminine adjective"
+          }},
+          {{
+            word.grammar.adjective.number === "s"
+              ? "Singular adjective"
+              : "Plural adjective"
+          }}
+        </div>
+        <div v-if="word.grammar.verb">Verb</div>
+        <div v-if="word.grammar.pronoun">Pronoun</div>
+      </div>
       <!-- <div v-if="word.conjugation && !word.conjugation.category">
         <h2
           class="tw-text-pink-800 tw-text-xl sm:tw-text-3xl tw-mt-4 tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 lg:tw--ml-2 tw-pl-3 tw-shadow-md"
