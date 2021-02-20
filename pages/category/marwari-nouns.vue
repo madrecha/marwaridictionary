fa9c1aed78d6d9d5fa18635dd4213682ba7d0fb8
manasmadrecha <madrechamanas@gmail.com>
2021-02-21 00:43:42 +0530

    created temporary page to check grammar

---
+++ pages/category/marwari-nouns.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="word in words" :key="word">
+        {{ word.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      words: [],
+    };
+  },
+  async fetch() {
+    this.words = await this.$content("words")
+      .where({ grammar: { $contains: "noun" } })
+      .fetch();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
