52ce2d6993c24ba58d2dd111715dc1822390383b
manasmadrecha <madrechamanas@gmail.com>
2021-02-21 00:42:59 +0530

    antonym, synonym

---
+++ components/global/Ant.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="a in ant" :key="a">
+        <nuxt-link :to="`/word/${a}`">
+          {{ a }}
+        </nuxt-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { ant: Array },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
---
+++ components/global/Syn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ol>
+      <li v-for="s in syn" :key="s">
+        <nuxt-link :to="`/word/${s}`">
+          {{ s }}
+        </nuxt-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { syn: Array },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
