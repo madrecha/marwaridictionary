f0225ad0a06386e64a2ed99939d2cc4aca529382
manasmadrecha <madrechamanas@gmail.com>
2021-02-21 02:37:23 +0530

    created common verb conj component

---
+++ components/global/verbConj.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <verb-conj-ending-a
+      v-if="grammar.verb.ending === 'a'"
+      :word="grammar.verb.term"
+      :worden="grammar.verb.trans"
+    ></verb-conj-ending-a>
+    <verb-conj-ending-aa
+      v-if="grammar.verb.ending === 'aa'"
+      :word="grammar.verb.term"
+      :worden="grammar.verb.trans"
+    ></verb-conj-ending-aa>
+  </div>
+</template>
+
+<script>
+import verbConjEndingA from "./grammar/verbConjEndingA.vue";
+import verbConjEndingAa from "./grammar/verbConjEndingAa.vue";
+
+export default {
+  props: { grammar: Object },
+  components: { verbConjEndingA, verbConjEndingAa },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
