7a97731cfe536f53d9ffc08158d787d3afb29af5
manasmadrecha <madrechamanas@gmail.com>
2021-02-23 00:09:55 +0530

    renamed sub-Components, capitalised

--- components/global/verbConj.vue
+++ components/global/verbConj.vue
@@ -2,34 +2,38 @@
   <div
     class="tw-max-w-xs tw-overflow-x-scroll sm:tw-overflow-x-auto sm:tw-max-w-full"
   >
-    <verb-conj-ending-a
+    <VerbConjEndingA
       v-if="grammar.verb.ending === 'a'"
       :term="grammar.verb.term"
       :trans="grammar.verb.trans"
-    ></verb-conj-ending-a>
-    <verb-conj-ending-aa
+    ></VerbConjEndingA>
+    <VerbConjEndingAa
       v-if="grammar.verb.ending === 'aa'"
       :term="grammar.verb.term"
       :trans="grammar.verb.trans"
-    ></verb-conj-ending-aa>
-    <verb-conj-ending-ae
+    ></VerbConjEndingAa>
+    <VerbConjEndingAe
       v-if="grammar.verb.ending === 'ae'"
       :term="grammar.verb.term"
       :trans="grammar.verb.trans"
       :term2="grammar.verb.term2"
       :trans2="grammar.verb.trans2"
-    ></verb-conj-ending-ae>
+    ></VerbConjEndingAe>
   </div>
 </template>
 
 <script>
-import verbConjEndingA from "./grammar/verbConjEndingA.vue";
-import verbConjEndingAa from "./grammar/verbConjEndingAa.vue";
-import verbConjEndingAe from "./grammar/verbConjEndingAe.vue";
+import VerbConjEndingA from "../grammar/VerbConjEndingA.vue";
+import VerbConjEndingAa from "../grammar/VerbConjEndingAa.vue";
+import VerbConjEndingAe from "../grammar/VerbConjEndingAe.vue";
 
 export default {
   props: { grammar: Object },
-  components: { verbConjEndingA, verbConjEndingAa, verbConjEndingAe },
+  components: {
+    VerbConjEndingA,
+    VerbConjEndingAa,
+    VerbConjEndingAe,
+  },
   data() {
     return {};
   },
