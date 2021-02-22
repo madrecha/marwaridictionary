94dc0df954a8aea6e0820d212d0cabe7de78b9a6
manasmadrecha <madrechamanas@gmail.com>
2021-02-22 22:56:46 +0530

    removed comments, and moved categories to component

---
+++ components/MaintenanceCategories.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div v-if="word.grammar" class="tw-mt-8">
+      <h2
+        class="tw-text-pink-800 tw-text-xl sm:tw-text-3xl tw-mt-4 tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 lg:tw--ml-2 tw-pl-3 tw-shadow-md"
+      >
+        Meta categories
+      </h2>
+      <div v-if="word.grammar.noun">
+        Noun,
+        {{
+          word.grammar.noun.gender === "m" ? "Masculine Noun" : "Feminine Noun"
+        }},
+        {{ word.grammar.noun.number === "s" ? "Singular Noun" : "Plural Noun" }}
+      </div>
+      <div v-if="word.grammar.adjective">
+        Adjective,
+        {{
+          word.grammar.adjective.gender === "m"
+            ? "Masculine adjective"
+            : "Feminine adjective"
+        }},
+        {{
+          word.grammar.adjective.number === "s"
+            ? "Singular adjective"
+            : "Plural adjective"
+        }}
+      </div>
+      <div v-if="word.grammar.verb">Verb</div>
+      <div v-if="word.grammar.pronoun">Pronoun</div>
+    </div>
+    <div v-if="word.examples">Words with Examples</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: { word: Object },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
--- pages/word/_slugurl.vue
+++ pages/word/_slugurl.vue
@@ -5,56 +5,7 @@
         {{ word.title }} ({{ word.transliteration }}) meaning in Marwari
       </h1>
       <NuxtContent :document="word"></NuxtContent>
-      <div v-if="word.grammar" class="tw-mt-8">
-        <h2
-          class="tw-text-pink-800 tw-text-xl sm:tw-text-3xl tw-mt-4 tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 lg:tw--ml-2 tw-pl-3 tw-shadow-md"
-        >
-          Meta categories
-        </h2>
-        <div v-if="word.grammar.noun">
-          Noun,
-          {{
-            word.grammar.noun.gender === "m"
-              ? "Masculine Noun"
-              : "Feminine Noun"
-          }},
-          {{
-            word.grammar.noun.number === "s" ? "Singular Noun" : "Plural Noun"
-          }}
-        </div>
-        <div v-if="word.grammar.adjective">
-          Adjective,
-          {{
-            word.grammar.adjective.gender === "m"
-              ? "Masculine adjective"
-              : "Feminine adjective"
-          }},
-          {{
-            word.grammar.adjective.number === "s"
-              ? "Singular adjective"
-              : "Plural adjective"
-          }}
-        </div>
-        <div v-if="word.grammar.verb">Verb</div>
-        <div v-if="word.grammar.pronoun">Pronoun</div>
-      </div>
-      <div v-if="word.examples">Words with Examples</div>
-      <!-- <div v-if="word.conjugation && !word.conjugation.category">
-        <h2
-          class="tw-text-pink-800 tw-text-xl sm:tw-text-3xl tw-mt-4 tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 lg:tw--ml-2 tw-pl-3 tw-shadow-md"
-        >
-          Conjugation
-        </h2>
-        <div
-          class="tw-mt-3 tw-max-w-xs tw-overflow-x-scroll sm:tw-overflow-x-auto sm:tw-max-w-full"
-        >
-          <MarwariVerbConjugation
-            :word="word.conjugation.term"
-            :worden="word.conjugation.en"
-            :wordcat="word.conjugation.category"
-          ></MarwariVerbConjugation>
-        </div> 
-      </div> -->
+      <MaintenanceCategories :word="word"></MaintenanceCategories>
     </div>
   </div>
 </template>
