24e0c9173afca3e559e2f9aa371ceef22b46ed9a
manasmadrecha <madrechamanas@gmail.com>
2021-02-23 08:00:28 +0530

    added 404 fallback page in generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  generate: {
+    fallback: '404.html',
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
