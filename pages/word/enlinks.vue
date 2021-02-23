6558c77964eae406921f2c2a86b6845084a6bd3b
manasmadrecha <madrechamanas@gmail.com>
2021-02-23 06:59:08 +0530

    added english links page to generate enlinks

---
+++ pages/word/enlinks.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <!-- This page is to generate Link of the words based on their file name which is denoted by "slug" -->
+    <ul v-for="word in words" :key="word.title">
+      <li>
+        <nuxt-link :to="`/word/${word.slug}`">{{ word.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      words: [],
+    };
+  },
+  async fetch() {
+    this.words = await this.$content("words")
+      .where({ slug: { $ne: "README" } })
+      .without(["body", "toc"])
+      .sortBy("transliteration")
+      .fetch();
+  },
+};
+</script>
