f2ed7c61679ab6d1383d0759403cb6c6f1cae24a
manasmadrecha <madrechamanas@gmail.com>
2021-02-24 02:20:18 +0530

    test component (delete it later)

---
+++ components/NounDeclTrial.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ grammar }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: { grammar: Object },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
