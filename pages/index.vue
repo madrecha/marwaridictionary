66675acc1e9b7e052baf213d2553110e9406a5a4
manasmadrecha <madrechamanas@gmail.com>
2021-02-23 05:42:53 +0530

    better responsive, coloring

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,74 @@
 <template>
   <div class="tw-mx-auto">
-    <div id="first-section" class="tw-text-center">
-      <div class="tw-max-w-4xl tw-mt-2 sm:tw-mt-6 tw-mx-auto">
-        <div class="sm:tw-flex sm:tw-justify-center">
-          <div class="sm:tw-w-1/3">
-            <girl-learning-paper-letters
-              class="tw-w-48 tw-h-48 sm:tw-w-60 sm:tw-h-60 tw-mx-auto"
-            ></girl-learning-paper-letters>
-          </div>
-          <h1
-            class="sm:tw-w-2/3 tw-mt-1 sm:tw-mt-3 tw-text-5xl md:tw-text-8xl tw-text-blue-900 tw-tracking-wide"
-          >
-            Marwari Dictionary
-          </h1>
-        </div>
-        <h2
-          class="tw-mt-8 sm:tw-mt-0 tw-text-3xl md:tw-text-6xl tw-text-pink-700 tw-leading-3"
-        >
-          Learn Marwari language
-        </h2>
-      </div>
+    <div id="first-section" class="tw-text-center tw-mt-2">
       <div
-        class="tw-mt-16 sm:tw-mt-16 tw-border-t-4 tw-border-dashed tw-border-blue-800"
+        class="tw-pb-20 tw-border-b-2 tw-border-dashed tw-border-blue-800 tw-bg-gradient-to-br tw-from-white tw-via-white tw-to-pink-50"
       >
+        <div class="tw-max-w-4xl tw-mx-auto">
+          <div class="md:tw-flex md:tw-justify-center">
+            <div class="md:tw-w-1/3">
+              <svg-girl-learning-paper-letters
+                class="tw-w-48 tw-h-48 md:tw-w-60 md:tw-h-60 tw-mx-auto"
+              ></svg-girl-learning-paper-letters>
+            </div>
+            <h1
+              class="md:tw-w-2/3 tw-mt-1 md:tw-mt-3 tw-text-5xl md:tw-text-8xl tw-text-blue-900 tw-tracking-wide"
+            >
+              Marwari Dictionary
+            </h1>
+          </div>
+          <h2
+            class="tw-mt-8 md:tw-mt-0 tw-text-3xl md:tw-text-6xl tw-text-pink-700 tw-leading-3"
+          >
+            Learn Marwari language
+          </h2>
+        </div>
+      </div>
+      <div class="tw--mt-5">
         <nuxt-link
           to="/word"
-          class="tw-p-6 tw-rounded-xl tw-text-white tw-font-medium tw-bg-gradient-to-br tw-from-pink-500 tw-to-pink-700 hover:tw-from-white hover:tw-to-pink-100 hover:tw-shadow-2xl tw-border-t-2 hover:tw-border-pink-200 hover:tw-text-pink-800 tw-text-xl"
+          class="tw-p-6 tw-rounded-xl tw-text-white tw-font-medium tw-bg-gradient-to-br tw-from-pink-500 tw-to-pink-700 hover:tw-from-white hover:tw-to-pink-100 tw-shadow-xl tw-border-t-2 hover:tw-border-pink-200 hover:tw-text-pink-800 tw-text-xl"
           >Browse Marwari Words</nuxt-link
         >
       </div>
-      <div class="tw-max-w-4xl tw-mt-12 tw-mx-auto">
-        <p class="tw-text-2xl tw-text-pink-900">
-          A place to learn Marwari / Marwadi, Rajasthani, Mewadi and improve
-          your vocabulary of Marwari words.
-        </p>
-        <p class="tw-mt-3 tw-text-xl tw-text-gray-700">
-          If you already know better, then
-          <a
-            href="https://github.com/madrecha/marwaridictionary"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="tw-text-blue-800 tw-border-b tw-border-blue-800"
-          >
-            join us</a
-          >
-          to help others learn.
-        </p>
+      <div
+        class="tw-py-2 tw-mt-10 md:tw-mt-3 md:tw-flex tw-justify-center tw-items-start tw-bg-gradient-to-br tw-from-white tw-via-white tw-to-blue-50"
+      >
+        <div
+          class="tw-px-3 md:tw-w-10/12 md:tw-flex tw-justify-center tw-items-center md:tw-pt-8"
+        >
+          <p class="tw-text-2xl tw-text-pink-900">
+            A place to learn Marwari / Marwadi, Rajasthani, Mewadi and improve
+            your vocabulary of Marwari words.
+          </p>
+          <p class="tw-mt-3 md:tw-mt-0 tw-text-xl tw-text-gray-700">
+            If you already know better, then
+            <a
+              href="https://github.com/madrecha/marwaridictionary"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="tw-text-blue-800 tw-border-b tw-border-blue-800"
+            >
+              join us</a
+            >
+            to help others learn.
+          </p>
+        </div>
+        <div class="md:tw-w-2/12 md:tw-px-2 md:tw--mt-4 tw-mt-3">
+          <svg-father-son
+            class="tw-w-48 tw-h-36 md:tw-w-40 lg:tw-w-48 tw-mx-auto"
+          ></svg-father-son>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import GirlLearningPaperLetters from "~/components/svg/girl-learning-paper-letters.vue";
+import SvgGirlLearningPaperLetters from "~/components/svg/svg-girl-learning-paper-letters.vue";
+import SvgFatherSon from "~/components/svg/svg-father-son.vue";
+
 export default {
-  components: { GirlLearningPaperLetters },
+  components: { SvgGirlLearningPaperLetters, SvgFatherSon },
 };
 </script>
-
-<style lang="postcss" scoped>
-.title {
-  girllearningpaperlettersfont-family: "Quicksand", "Source Sans Pro",
-    -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue",
-    Arial, sans-serif;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
