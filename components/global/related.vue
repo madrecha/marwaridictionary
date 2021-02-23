46f648faf0bb042056ee70d3981442dab04ca899
manasmadrecha <madrechamanas@gmail.com>
2021-02-24 02:21:19 +0530

    component for related words

---
+++ components/global/related.vue
@@ -0,0 +1,30 @@
+<template>
+  <ol>
+    <li v-for="each_related in related" :key="each_related.rel">
+      <span v-if="each_related.context" class="tw-text-sm"
+        >({{ each_related.context }})</span
+      >
+      <nuxt-link
+        :to="
+          each_related.slugurl
+            ? `/word/${each_related.slugurl}`
+            : `/word/${each_related.rel}`
+        "
+      >
+        {{ each_related.rel }}
+      </nuxt-link>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: { related: Array, url: Object },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
