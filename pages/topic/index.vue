45a2fd3550eed9eeda45c3a2fb05dbfd9a8bab68
manasmadrecha <madrechamanas@gmail.com>
2021-02-25 02:10:11 +0530

    completely changed code for only added+unique topics

--- pages/topic/index.vue
+++ pages/topic/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Topics</h1>
-    <ol>
+    <!-- <ol>
       <li v-for="(topic, i) in topics" :key="i">
         <nuxt-link
           :to="`/topic/${topic.url.slugurl}`"
@@ -11,13 +11,18 @@
           {{ topic.url.slugurl }}
         </nuxt-link>
 
-        <!-- <ol v-if="topic.children && topic.children.length > 0">
+        <ol v-if="topic.children && topic.children.length > 0">
           <li v-for="children in topic.children" :key="children">
             <nuxt-link :to="`/topic/${children}`" class="tw-capitalize">
               {{ children }}
             </nuxt-link>
           </li>
-        </ol> -->
+        </ol>
+      </li>
+    </ol> -->
+    <ol>
+      <li v-for="uniquetopic in uniquetopics" :key="uniquetopic">
+        {{ uniquetopic }}
       </li>
     </ol>
   </div>
@@ -28,10 +33,32 @@ export default {
   data() {
     return {
       topics: [],
+      words: [],
+      alltopics: [],
+      uniquetopics: [],
     };
   },
   async fetch() {
     this.topics = await this.$content("topics").sortBy("slug").fetch();
+
+    this.words = await this.$content("words")
+      .only("topics")
+      .sortBy(["slug"])
+      .fetch();
+    for (const word of this.words) {
+      if (word.topics) {
+        for (const topic of word.topics) {
+          this.alltopics.push(topic);
+        }
+      }
+    }
+
+    this.uniquetopics = this.alltopics
+      .reduce(function (a, b) {
+        if (a.indexOf(b) < 0) a.push(b);
+        return a;
+      }, [])
+      .sort();
   },
 };
 </script>
