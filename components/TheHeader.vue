21a345f697dc195f9c930b799b4e7c68ae407551
manasmadrecha <madrechamanas@gmail.com>
2021-02-25 00:32:00 +0530

    better css

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,22 +1,25 @@
 <template>
   <div
-    class="tw-sticky tw-top-0 tw-z-30 tw-flex tw-items-center tw-justify-center tw-bg-gradient-to-br tw-from-white tw-to-blue-50"
+    class="tw-sticky tw-top-0 tw-z-30 tw-h-12 tw-flex tw-items-center tw-justify-start tw-bg-gradient-to-br tw-from-white tw-to-blue-50"
   >
     <div class="tw-w-1/2 sm:tw-w-1/3">
-      <p
-        class="tw-p-3 tw-text-center tw-font-medium sm:tw-text-2xl tw-text-pink-900"
-      >
+      <p class="tw-text-center tw-font-medium sm:tw-text-2xl tw-text-pink-900">
         Marwari Dictionary
       </p>
     </div>
     <nav class="tw-w-1/2 sm:tw-w-2/3 tw-px-2 sm:tw-px-8">
-      <ul class="tw-p-2 tw-mx-auto tw-flex tw-list-none">
+      <ul class="tw-flex">
         <li
           v-for="menu in menus"
           :key="menu.name"
-          class="tw-text-sm sm:tw-text-base tw-p-1.5 sm:tw-p-3 hover:tw-bg-pink-50 tw-font-medium"
+          class="tw-text-sm sm:tw-text-base tw-mx-2 tw-font-medium"
         >
-          <nuxt-link :to="menu.url">{{ menu.name }}</nuxt-link>
+          <nuxt-link
+            exact
+            :to="menu.url"
+            class="tw-p-2.5 hover:tw-bg-pink-50"
+            >{{ menu.name }}</nuxt-link
+          >
         </li>
       </ul>
     </nav>
@@ -40,6 +43,6 @@ export default {
 
 <style lang="postcss" scoped>
 .nuxt-link-exact-active {
-  @apply tw-bg-pink-50 tw-p-3  tw-rounded;
+  @apply tw-border-pink-500 tw-border tw-rounded-xl tw-bg-white;
 }
 </style>
