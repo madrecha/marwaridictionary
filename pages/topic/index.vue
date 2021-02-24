6e9152cfd98033627283af1ffb12a26409b9876b
manasmadrecha <madrechamanas@gmail.com>
2021-02-24 23:33:19 +0530

    created topic index page to fetch all topics files

--- pages/topic/index.vue
+++ pages/topic/index.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>Topic Index page</div>
+  <div>
+    <h1>Topics</h1>
+    <ol>
+      <li v-for="(topic, i) in topics" :key="i">
+        <nuxt-link :to="`/topic/${topic.url.slugurl}`" class="tw-capitalize">
+          {{ topic.url.slugurl }}
+        </nuxt-link>
+
+        <ol v-if="topic.children && topic.children.length > 0">
+          <li v-for="children in topic.children" :key="children">
+            <nuxt-link :to="`/topic/${children}`" class="tw-capitalize">
+              {{ children }}
+            </nuxt-link>
+          </li>
+        </ol>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      topics: [],
+    };
+  },
+  async fetch() {
+    this.topics = await this.$content("topics").fetch();
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="postcss" scoped>
+@import url(~assets/css/nuxtcontent.postcss);
 </style>
