c31210f6964f2ad8f1a420f181b0524bc618095b
manasmadrecha <madrechamanas@gmail.com>
2021-02-25 14:18:05 +0530

    css, bg color, font-size, seo h3 h4 titles

--- components/global/meaning.vue
+++ components/global/meaning.vue
@@ -2,29 +2,79 @@
   <div>
     <p v-if="url">
       Marwari meaning of
-      <span class="tw-capitalize">{{ url.transliteration }}</span>
+      <span class="tw-capitalize"
+        >{{ url.title }} ({{ url.transliteration }})</span
+      >
     </p>
     <ol>
-      <li v-for="mean in meanings" :key="mean.meaning">
-        <span v-html="mean.meaning" class="tw-text-2xl"></span>
+      <li v-for="mean in meanings" :key="mean.meaning" class="tw-mt-2">
+        <span
+          v-html="mean.meaning"
+          class="tw-text-2xl tw-p-2 tw-bg-gradient-to-br tw-from-white tw-to-pink-50"
+        ></span>
 
-        <h3 v-if="(mean.examples || mean.egs) && h === 2">Marwari Examples</h3>
-        <h4 v-if="(mean.examples || mean.egs) && h === 3">Marwari Examples</h4>
+        <h3 v-if="(mean.examples || mean.egs) && h === 2" class="tw-text-base">
+          Examples of
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+          in Marwari
+        </h3>
+        <h4 v-if="(mean.examples || mean.egs) && h === 3" class="tw-text-base">
+          Examples of
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+          in Marwari
+        </h4>
         <eg :eg="mean.examples" v-if="mean.examples"></eg>
         <eg :eg="mean.egs" v-if="mean.egs"></eg>
 
-        <h3 v-if="(mean.synonyms || mean.syns) && h === 2">Marwari Synonyms</h3>
-        <h4 v-if="(mean.synonyms || mean.syns) && h === 3">Marwari Synonyms</h4>
+        <h3
+          v-if="(mean.synonyms || mean.syns) && h === 2"
+          class="tw-text-base sm:tw-text-xl"
+        >
+          Marwari synonyms of
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+        </h3>
+        <h4 v-if="(mean.synonyms || mean.syns) && h === 3" class="tw-text-base">
+          Marwari synonyms of
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+        </h4>
         <syn :syn="mean.synonyms" v-if="mean.synonyms"></syn>
         <syn :syn="mean.syns" v-if="mean.syns"></syn>
 
-        <h3 v-if="(mean.antonyms || mean.ants) && h === 2">Marwari Antonyms</h3>
-        <h4 v-if="(mean.antonyms || mean.ants) && h === 3">Marwari Antonyms</h4>
+        <h3 v-if="(mean.antonyms || mean.ants) && h === 2" class="tw-text-base">
+          Marwari antonyms of
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+        </h3>
+        <h4 v-if="(mean.antonyms || mean.ants) && h === 3" class="tw-text-base">
+          Marwari antonyms of
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+        </h4>
         <ant :ant="mean.antonyms" v-if="mean.antonyms"></ant>
         <ant :ant="mean.ants" v-if="mean.ants"></ant>
 
-        <h3 v-if="(mean.related || mean.rels) && h === 2">Related</h3>
-        <h4 v-if="(mean.related || mean.rels) && h === 3">Related</h4>
+        <h3 v-if="(mean.related || mean.rels) && h === 2" class="tw-text-base">
+          Marwari words related to
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+        </h3>
+        <h4 v-if="(mean.related || mean.rels) && h === 3" class="tw-text-base">
+          Marwari words related to
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
+        </h4>
         <related :related="mean.related" v-if="mean.related"></related>
         <related :related="mean.rels" v-if="mean.rels"></related>
 
@@ -33,25 +83,34 @@
             (mean.translation || mean.translations || mean.translates) &&
             h === 2
           "
+          class="tw-text-base"
         >
-          Translation
+          Translate Marwari word
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
         </h3>
         <h4
           v-if="
             (mean.translation || mean.translations || mean.translates) &&
             h === 3
           "
+          class="tw-text-base"
         >
-          Translation
+          Translate Marwari word
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
         </h4>
         <p
           v-if="
             url && (mean.translation || mean.translations || mean.translates)
           "
         >
-          Translate
-          <span class="tw-capitalize">{{ url.transliteration }}</span> in
-          Marwari
+          Translate Marwari word
+          <span class="tw-capitalize"
+            >{{ url.title }} ({{ url.transliteration }})</span
+          >
         </p>
         <translation
           :translation="mean.translation"
