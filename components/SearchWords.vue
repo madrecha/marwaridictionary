a6375691f68e43953764667ab84aaeb4684cc889
manasmadrecha <madrechamanas@gmail.com>
2021-02-26 11:58:43 +0530

    remove query on click method, width prop

--- components/SearchWords.vue
+++ components/SearchWords.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-    <div class="tw-flex tw-flex-col">
-      <div>
-        <input
-          v-model="query"
-          type="search"
-          autocomplete="off"
-          placeholder="Search..."
-          class="tw-border tw-rounded-xl tw-shadow-lg focus:tw-outline-none focus:tw-ring-2 tw-py-2 tw-px-3"
-        />
-      </div>
-      <ul v-if="wordsToSearch.length" class="tw-mt-3">
-        <li v-for="word of wordsToSearch" :key="word.slug">
+  <div class="tw-relative">
+    <div>
+      <input
+        id="search-word-input"
+        v-model="query"
+        type="search"
+        autocomplete="off"
+        placeholder="Search..."
+        class="tw-border tw-rounded-xl tw-shadow-lg focus:tw-outline-none focus:tw-ring-2 tw-py-1 tw-px-3"
+        :class="width"
+      />
+    </div>
+    <div
+      class="tw-mt-3 tw-max-w-3xl tw-mx-auto tw-absolute tw-top-6 tw-left-0 tw-right-0"
+    >
+      <ul
+        v-if="wordsToSearch.length"
+        class="tw-bg-gradient-to-br tw-from-white tw-to-pink-50 tw-rounded tw-p-2"
+      >
+        <li
+          v-for="word of wordsToSearch"
+          :key="word.slug"
+          @click.prevent="clearInput()"
+        >
           <NuxtLink :to="`/word/${word.url.slugurl}`">
             {{ word.url.title }} ({{ word.url.transliteration }})
           </NuxtLink>
@@ -23,6 +34,7 @@
 
 <script>
 export default {
+  props: { width: { type: String, default: "tw-w-2/3" } },
   data() {
     return {
       wordsToSearch: [],
@@ -42,6 +54,14 @@ export default {
         .limit(10)
         .search(query)
         .fetch();
+    },
+  },
+  methods: {
+    clearInput() {
+      this.wordsToSearch = [];
+      // document.getElementById("search-word-input").value = "";
+      this.query = "";
+      return;
     },
   },
 };
