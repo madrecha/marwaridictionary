c8c35c4886bae03c2ae87d0e617314e348280952
manasmadrecha <madrechamanas@gmail.com>
2021-02-26 12:06:45 +0530

    temporary responsive, nuxt-link-active, hidemenu

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,18 +1,20 @@
 <template>
   <div
-    class="tw-sticky tw-top-0 tw-z-30 tw-h-12 tw-flex tw-items-center tw-justify-start tw-bg-gradient-to-br tw-from-white tw-to-blue-50"
+    class="tw-sticky tw-top-0 tw-z-30 tw-h-12 tw-flex tw-items-center tw-justify-center tw-bg-gradient-to-br tw-from-white tw-to-blue-50"
   >
-    <div class="tw-w-1/2 sm:tw-w-1/3">
-      <p class="tw-text-center tw-font-medium sm:tw-text-2xl tw-text-pink-900">
-        Marwari Dictionary
-      </p>
+    <div class="tw-w-1/5 md:tw-hidden tw-p-2">
+      <button @click.prevent="show = !show">🧾</button>
     </div>
-    <nav class="tw-w-1/2 sm:tw-w-2/3 tw-px-2 sm:tw-px-8">
-      <ul class="tw-flex">
+    <nav
+      class="md:tw-block md:tw-w-1/3 tw-px-2"
+      :class="!show ? ['tw-hidden'] : ['tw-block']"
+    >
+      <ul id="ul-menu" class="md:tw-flex tw-items-center tw-justify-center">
         <li
           v-for="menu in menus"
           :key="menu.name"
           class="tw-text-sm sm:tw-text-base tw-mx-0.5 md:tw-mx-2 tw-font-medium"
+          @click.prevent="hideMenu()"
         >
           <nuxt-link
             exact
@@ -23,6 +25,14 @@
         </li>
       </ul>
     </nav>
+    <div class="tw-w-2/5 md:tw-w-1/3">
+      <p class="tw-text-center tw-font-medium sm:tw-text-2xl tw-text-pink-900">
+        <nuxt-link to="/"> Marwari </nuxt-link>
+      </p>
+    </div>
+    <div class="tw-w-2/5 md:tw-w-1/3 tw-flex tw-items-center tw-justify-start">
+      <SearchWords :width="'tw-w-2/3 md:tw-w-full'"></SearchWords>
+    </div>
   </div>
 </template>
 
@@ -30,19 +40,25 @@
 export default {
   data() {
     return {
+      show: false,
       menus: [
-        { name: "Home", url: "/" },
+        // { name: "Home", url: "/" },
         { name: "Words", url: "/word" },
         { name: "Topics", url: "/topic" },
         // { name: "About", url: "/about" },
       ],
     };
   },
+  methods: {
+    hideMenu() {
+      this.show = false;
+    },
+  },
 };
 </script>
 
 <style lang="postcss" scoped>
-.nuxt-link-exact-active {
+#ul-menu .nuxt-link-exact-active {
   @apply tw-border-pink-500 tw-border tw-rounded-xl tw-bg-white;
 }
 </style>
