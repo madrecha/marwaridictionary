84f1e9955d160334b78198c22b4423de5c0b95cc
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-05-17 23:52:33 +0530

    word example component (slot-based)

---
+++ components/word/WordExample.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="word-example">
+    <dl class="tw-mt-3">
+      <dt
+        lang="mwr-Deva"
+        class="word-example--mwr tw-ml-3"
+      >
+        <slot name="mwr"></slot>
+      </dt>
+      <dd
+        lang="mwr-Latn"
+        class="word-example--mwr-Latn tw-mt-2 tw-ml-6"
+      >
+        <slot name="mwr-Latn"></slot>
+      </dd>
+      <dd
+        :lang="$i18n.locale"
+        class="word-example--locale tw-mt-2 tw-ml-6"
+      >
+        <slot :name="$i18n.locale"></slot>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WordExample",
+  data() {
+    return {};
+  },
+  methods: {
+    // transliterate() {
+    //   let marwariExample = document.querySelector(`dt[lang="mwr-Deva"]`);
+    //   console.log(marwariExample.innerHTML);
+    // }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.word-example--mwr
+  & > b
+    @apply tw-bg-yellow-50
+  & > strong
+    @apply tw-bg-yellow-50
+</style>
