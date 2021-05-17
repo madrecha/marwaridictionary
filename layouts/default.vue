699d7d0fb0278f73f0c776c3132b8d0af69cf696
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-05-18 01:05:22 +0530

    styling 1. bg color 2. para margin 3. root font size

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <TheHeader></TheHeader>
-    <main>
+    <main class="tw-bg-gray-50">
       <Nuxt
-        class="tw-max-w-screen-lg tw-mx-auto tw-py-4"
+        class="tw-max-w-screen-lg tw-mx-auto tw-px-2 tw-py-4 "
         keep-alive
         :keep-alive-props="{ max: 10 }"
       ></Nuxt>
