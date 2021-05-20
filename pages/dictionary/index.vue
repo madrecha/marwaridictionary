93c7c09f2aa6f60ea5453aaced0474df2f7ecf80
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-05-20 13:03:26 +0530

    style: index page of Dictionary + lists of dicts

--- pages/dictionary/index.vue
+++ pages/dictionary/index.vue
@@ -1,26 +1,50 @@
 <template>
   <div class="nuxt-content tw-max-w-screen-lg tw-mx-auto">
-    <h1 class="tw-text-center tw-text-2xl tw-text-pink-900 tw-font-medium">Marwari Dictionary</h1>
-    <section class="tw-flex tw-justify-evenly tw-items-center">
+    <h1>Marwari Dictionaries</h1>
+    <p>While Marwari Dictionaries can be monolingual or bilingual, we have started with preparing a <strong>bilingual Marwari to English dictionary</strong>. Why? Because we feel that with this approach, we can quickly reach out to the English speakers who wish to learn Marwari language.</p>
+    <p>Of course, we will also try to prepare a bilingual English to Marwari dictionary. You too can help us to contribute to the community.</p>
+    <p></p>
+    <section>
+      <h2>List of Marwari Dictionaries</h2>
+      <ol>
+        <li class="tw-">
+          <nuxt-link :to="`/${$i18n.locale}/dictionary/marwari-english/`">Marwari-English dictionary</nuxt-link>
+        </li>
+        <li class="tw-text-gray-500">English-Marwari dictionary (WIP)</li>
+        <li class="tw-text-gray-500">Marwari-Hindi dictionary (planned)</li>
+        <li class="tw-text-gray-500">Hindi-Marwari dictionary (planned)</li>
+        <li class="tw-text-gray-500">Marwari dictionary (planned) (monolingual)</li>
+      </ol>
+      <p class="tw-text-sm">WIP = Work in Progress</p>
+    </section>
 
-      <b-button
+    <section>
+      <h2>Long-term Vision</h2>
+      <p>Read our <a
+          href="https://github.com/madrecha/marwaridictionary/blob/master/PLAN.md"
+          ref="noopener noreferer"
+          target="_blank"
+        >plan on Github</a> on how we aim to implement these extensively comphrensive Marwari Dictionaries.</p>
+    </section>
+
+    <!-- <section class="tw-flex tw-justify-evenly tw-items-center">
+
+      <nuxt-link
         class="tw-w-48 tw-h-48 tw-rounded-lg tw-border tw-border-gray-200 tw-bg-yellow-50 tw-flex tw-justify-center tw-items-center"
         :to="`/${$i18n.locale}/dictionary/word/`"
-        tag="nuxt-link"
       >
         Words
-      </b-button>
-      <b-button
+      </nuxt-link>
+      <nuxt-link
         class="tw-w-48 tw-h-48 tw-rounded-lg tw-border tw-border-gray-200 tw-bg-yellow-50 tw-flex tw-justify-center tw-items-center"
         :to="`/${$i18n.locale}/dictionary/topic/`"
-        tag="nuxt-link"
       >
         Topics
-      </b-button>
-    </section>
-    <article>
+      </nuxt-link>
+    </section> -->
+    <!-- <article>
       <nuxt-content :document="post"></nuxt-content>
-    </article>
+    </article> -->
 
   </div>
 </template>
