e6dbe158bac173926582855e73c1c455e25afeba
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-05-26 19:49:18 +0530

    feat(word): new Word Qualifier component 1. to write additional info just besides a meaning 2. it is a slot-based component

---
+++ components/word/WordQualifier.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="word-qualifier-root-div">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.word-qualifier-root-div
+  @apply tw-inline tw-text-sm tw-text-gray-500
+  &::before
+    content: "("
+  &::after
+    content: ")"
+.word-qualifier-root-div *
+  @apply tw-inline tw-text-sm tw-text-gray-500
+</style>
