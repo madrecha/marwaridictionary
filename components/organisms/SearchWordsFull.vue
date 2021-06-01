a694de219389798dd35ef2d84c533738fc946187
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-06-01 20:53:53 +0530

    feat: TheHeader and Search revamped with vuetify 1. toggleable Aside Nav 2. better responsiveness 3. social media icons 4. styling 5. icons 6. also search in transliteration_alt 7. in smAndDown, open dialog for search

--- components/organisms/SearchWordsFull.vue
+++ components/organisms/SearchWordsFull.vue
@@ -1,73 +1,76 @@
 <template>
   <div class="tw-relative">
-    <div>
-      <input
-        autofocus
-        v-model="query"
-        type="text"
-        autocomplete="off"
-        placeholder="Search..."
-        class="tw-border tw-rounded-xl tw-shadow focus:tw-outline-none focus:tw-ring-2 tw-py-1 tw-px-3"
-        :class="width"
-        @keyup.esc="clearInput()"
-        v-click-outside="clearInput"
-        @click.prevent="showSearchHelp = true"
-      />
-      <!-- @mouseover="showSearchHelp = true"
+    <input
+      type="search"
+      label="Search"
+      v-model="query"
+      autocomplete="off"
+      autofocus
+      placeholder="Search for word 🔎"
+      class="tw-bg-white tw-rounded-lg tw-p-2 tw-shadow tw-border tw-border-gray-700 focus:tw-outline-none focus:tw-ring-2 tw-ring-blue-400 tw-transform tw-transition-transform"
+      :class="width"
+      transition="slide-x-transition"
+      @keyup.esc="clearInput()"
+      v-click-outside="clearInput"
+      @click.prevent="showSearchHelp = true"
+    />
+    <!-- @mouseover="showSearchHelp = true"
         @mouseleave="showSearchHelp = false" -->
-    </div>
     <div
-      class="tw-absolute tw-top-10 tw-left-0 tw-right-0 tw-flex tw-flex-wrap tw-bg-yellow-50 tw-p-1 tw-rounded tw-shadow"
+      class="tw-absolute tw-top-12 tw-left-0 tw-right-0 tw-flex tw-flex-wrap tw-bg-white tw-p-1 tw-rounded tw-shadow"
+      v-if="showSearchHelp && results.length < 1"
       :class="!showSearchHelp ? 'tw-hidden' : 'tw-block'"
     >
-      <div class="tw-relative">
-        <div>
-          <p class="tw-py tw-pl-1 tw-pr-4 tw-text-xs">
-            Search Word by Title, Topic, Label
-            <br />
-            <span class="tw-text-xs">E.g. → विंदणी, vindani, person</span>
-            <br />
-            <span class="tw-text-xs">E.g. → कुत्री, kutro, animal, abusive</span>
-          </p>
-        </div>
-        <button
-          @click="showSearchHelp = false"
-          class="tw-absolute tw-top-0 tw-right-2 tw-rounded-full tw-text-lg tw-text-red-500 hover:tw-bg-yellow-100 tw-px-2 focus:tw-outline-none"
-        >
-          ×
-        </button>
+      <div class="tw-py tw-pl-1 tw-pr-4 tw-text-xs">
+        Search <b>Word</b> by Title, Topic, Label
+        <br />
+        <span class="tw-text-xs">E.g. → विंदणी, vindani, person</span>
+        <br />
+        <span class="tw-text-xs">E.g. → कुत्री, kutro, animal, abusive</span>
       </div>
-    </div>
-    <div class="tw-mt-3 tw-max-w-3xl tw-mx-auto tw-absolute tw-top-6 tw-left-0 tw-right-0">
-      <ol
-        v-if="results.length"
-        class="tw-list-decimal tw-list-inside tw-bg-gradient-to-br tw-from-white tw-to-pink-50 tw-rounded tw-p-2 tw-z-40"
+      <button
+        @click="showSearchHelp = false"
+        class="tw-absolute tw-top-0 tw-right-2 tw-rounded-full tw-text-lg tw-text-red-500 hover:tw-bg-gray-50 tw-px-2 focus:tw-outline-none"
       >
+        ×
+      </button>
+    </div>
+    <div
+      class="tw-absolute tw-top-11 tw-max-w-3xl tw-mx-auto tw-left-0 tw-right-0 tw-max-h-80 tw-overflow-y-scroll tw-overscroll-x-auto tw-bg-gradient-to-br tw-from-white tw-to-white tw-shadow-md "
+      v-if="results.length"
+    >
+      <ol class="tw-ml-4 tw-list-decimal tw-list-outside tw-rounded tw-p-2 tw-z-40 ">
         <li
           v-for="word of results"
           :key="word.item.slug"
           @click.prevent="clearInput()"
-          class="tw-m-1 tw-z-40"
+          @click="$emit('linkClicked')"
+          class="tw-z-40"
         >
-          <NuxtLink
+          <nuxt-link
             :to="`/${$i18n.locale}/dictionary/marwari-english/${word.item.slugurl}/`"
-            class="focus:tw-ring-2 tw-p-1"
+            class="tw-inline-block tw-p-2 !tw-text-gray-600 hover:tw-bg-pink-600 hover:!tw-text-white focus:tw-bg-pink-600 focus:!tw-text-white focus:tw-outline-none"
           >
-            {{ word.item.title ? word.item.title : word.item.slugurl }} ({{ word.item.transliteration ? word.item.transliteration : null}})
-          </NuxtLink>
+            {{ word.item.title ? word.item.title : word.item.slugurl }} <span class="tw-text-sm">({{ word.item.transliteration ? word.item.transliteration : null}})</span>
+          </nuxt-link>
         </li>
       </ol>
     </div>
+
   </div>
 </template>
 
 <script>
 import Fuse from "fuse.js";
 
+import { mdiMagnify } from "@mdi/js";
+
 export default {
   props: { width: { type: String, default: "tw-w-2/3" } },
   data() {
     return {
+      mdiMagnify,
+      toggleSearch: false,
       words: [],
       results: [],
       query: "",
@@ -84,6 +87,7 @@ export default {
         keys: [
           { name: "slugurl", weight: 2 },
           { name: "transliteration", weight: 2 },
+          { name: "transliteration_alt", weight: 1.5 },
           { name: "topics", weight: 1 },
           { name: "labels", weight: 1 }
         ]
@@ -104,8 +108,14 @@ export default {
       this.words = [];
     } else {
       this.words = await this.$content(`dictionary/marwari-english`)
-        .only(["slugurl", "transliteration", "labels", "topics"])
-        .where({ $and: [{ slug: { $ne: "AAA" } }, { type: { $ne: "pyq" } }] })
+        .only([
+          "slugurl",
+          "transliteration",
+          "transliteration_alt",
+          "labels",
+          "topics"
+        ])
+        .where({ $and: [{ slug: { $ne: "AAA" } }] })
         .fetch();
     }
   },
--- components/templates/TheHeader.vue
+++ components/templates/TheHeader.vue
@@ -1,86 +1,267 @@
 <template>
-  <header class="header-custom tw-sticky tw-top-0 tw-z-30 tw-h-12 tw-flex tw-items-center tw-justify-center ">
-    <!-- <div class="tw-w-1/5 md:tw-hidden tw-p-2">
-      <button @click.prevent="show = !show">🧾</button>
-    </div> -->
-    <nav class="tw-w-2/3 tw-px-2 tw-text-green-800">
-      <ul class="ul-menu md:tw-flex tw-items-center tw-justify-center">
-        <li class="tw-text-sm sm:tw-text-base tw-mx-0.5 md:tw-mx-2 tw-font-medium">
-          <nuxt-link
-            :to="`${localePath('/')}/`"
-            class="header-nav-link"
-          > 🏠 Home </nuxt-link>
-        </li>
-        <li class="tw-text-sm sm:tw-text-base tw-mx-0.5 md:tw-mx-2 tw-font-medium">
-          <nuxt-link
-            exact
-            :to="`/${$i18n.locale}/dictionary/`"
-            class="header-nav-link"
-          >📔 Dictionary</nuxt-link>
-        </li>
-        <li class="tw-text-sm sm:tw-text-base tw-mx-0.5 md:tw-mx-2 tw-font-medium">
-          <nuxt-link
-            exact
-            :to="`/${$i18n.locale}/browse/`"
-            class="header-nav-link"
-          >👀 Browse</nuxt-link>
-        </li>
-        <li class="tw-text-sm sm:tw-text-base tw-mx-0.5 md:tw-mx-2 tw-font-medium">
-          <nuxt-link
-            exact
-            :to="`/${$i18n.locale}/label/`"
-            class="header-nav-link"
-          >🏷 Labels</nuxt-link>
-        </li>
-        <li class="md:tw-block tw-text-sm sm:tw-text-base tw-mx-0.5 md:tw-mx-2 tw-font-medium">
-          <nuxt-link
-            exact
-            :to="`/${$i18n.locale}/topic/`"
-            class="header-nav-link"
-          >📦 Topics</nuxt-link>
-        </li>
+  <div>
+    <v-navigation-drawer
+      app
+      v-model="toggleMenu"
+      class="!tw-z-50"
+      clipped
+    >
+      <template #prepend>
+        <v-card v-if="$vuetify.breakpoint.smAndDown">
+          <v-card-title>
+            <nuxt-link
+              :to="localePath('/')"
+              class="pink--text text--darken-2"
+            >
+              <v-icon color="pink">{{mdiHome}}</v-icon>
 
+              Marwari Dictionary
+
+            </nuxt-link>
+          </v-card-title>
+        </v-card>
+      </template>
+
+      <ul class="nav tw-flex tw-flex-col tw-my-2 !tw-px-1">
+        <li
+          v-for="menu in menus"
+          :key="menu.text"
+          :class="`hover:${menu.bgcolor}`"
+          
+        >
+          <nuxt-link
+            exact
+            :to="localePath(menu.link)"
+            class="tw-block tw-p-4 !tw-text-gray-500"
+          >
+            <v-icon
+              left
+              :color="menu.color"
+            >{{ menu.icon }}</v-icon>{{ menu.text }}
+          </nuxt-link>
+        </li>
       </ul>
-    </nav>
-    <div class="tw-w-1/3 tw-flex tw-items-center tw-justify-start">
-      <SearchWordsFull :width="'tw-w-2/3 md:tw-w-full'"></SearchWordsFull>
-      <!-- <LanguageInput></LanguageInput> -->
-    </div>
-  </header>
+
+      <template #append>
+        <footer class="tw-p-2">
+          &copy; Manas Madrecha
+        </footer>
+        <div
+          class="tw-flex tw-mx-2"
+          v-if="$vuetify.breakpoint.smAndDown"
+        >
+          <a
+            href="https://github.com/madrecha/marwaridictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="tw-block tw-p-2 tw-rounded-xl hover:tw-bg-gray-50 hover:tw-shadow"
+            title="Marwari Dictionary Github repository"
+          >
+            <v-icon color="grey darken-3">{{mdiGithub}}</v-icon>
+          </a>
+
+          <a
+            href="https://instagram.com/ManasMadrecha"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="tw-block tw-p-2 tw-rounded-xl hover:tw-bg-pink-50 hover:tw-shadow"
+            title="Manas Madrecha Instagram follow"
+          >
+            <v-icon color="pink darken-3">{{mdiInstagram}}</v-icon>
+          </a>
+        </div>
+      </template>
+
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      class="!tw-z-40"
+      clipped-left
+      flat
+    >
+      <template #img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to right, white, #E0F7FA"
+        >
+        </v-img>
+      </template>
+
+      <v-app-bar-nav-icon
+        role="menu"
+        @click.stop="toggleMenu = !toggleMenu"
+        color="grey"
+      ></v-app-bar-nav-icon>
+
+      <div class="tw-w-full tw-flex tw-justify-between tw-items-center">
+        <div class="tw-w-[90%] md:tw-w-[30%] tw-mx-2">
+          <nuxt-link
+            :to="localePath('/')"
+            class="tw-block tw-p-2 !tw-text-pink-700 tw-font-semibold  tw-text-xl md:tw-text-2xl"
+          >Marwari Dictionary</nuxt-link>
+        </div>
+
+        <div
+          class="tw-w-[10%] tw-mx-2"
+          v-if="$vuetify.breakpoint.smAndDown"
+        >
+          <v-btn
+            text
+            icon
+            color="blue darken-4"
+            @click="toggleSearch = true"
+          >
+            <v-icon>{{mdiMagnify}}</v-icon>
+          </v-btn>
+        </div>
+
+        <div
+          class="tw-w-[60%] tw-mx-2"
+          v-if="$vuetify.breakpoint.mdAndUp"
+        >
+          <search-words-full width="tw-w-full"></search-words-full>
+        </div>
+
+        <div
+          class="tw-w-[10%] tw-flex tw-mx-2"
+          v-if="$vuetify.breakpoint.mdAndUp"
+        >
+          <a
+            href="https://github.com/madrecha/marwaridictionary"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="tw-block tw-p-2 tw-rounded-xl hover:tw-bg-gray-50 hover:tw-shadow"
+            title="Marwari Dictionary Github repository"
+          >
+            <v-icon color="grey darken-3">{{mdiGithub}}</v-icon>
+          </a>
+
+          <a
+            href="https://instagram.com/ManasMadrecha"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="tw-block tw-p-2 tw-rounded-xl hover:tw-bg-pink-50 hover:tw-shadow"
+            title="Manas Madrecha Instagram follow"
+          >
+            <v-icon color="pink darken-3">{{mdiInstagram}}</v-icon>
+          </a>
+        </div>
+      </div>
+
+    </v-app-bar>
+
+    <v-dialog
+      v-model="toggleSearch"
+      scrollable
+      v-if="$vuetify.breakpoint.smAndDown"
+      eager
+    >
+      <section class="tw-min-h-[80vh] tw-flex tw-flex-col tw-bg-white">
+
+        <h2 class="tw-p-2 tw-m-1 tw-text-center tw-text-xl tw-font-medium">Search for Marwari words </h2>
+
+        <!-- <search-words-full
+          @linkClicked="toggleSearch = false"
+          @searchButtonClicked="toggleSearch = true"
+        ></search-words-full> -->
+
+        <div class="tw-w-[80%] tw-mx-auto">
+          <search-words-full
+            width="tw-w-full"
+            @linkClicked="toggleSearch = false"
+          ></search-words-full>
+        </div>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          color="blue darken-1"
+          text
+          @click="toggleSearch = false"
+        >
+          Close
+        </v-btn>
+      </section>
+    </v-dialog>
+  </div>
+  <!-- <LanguageInput></LanguageInput> -->
 </template>
 
 <script>
 import LanguageInput from "~/components/organisms/LanguageInput";
 import SearchWordsFull from "~/components/organisms/SearchWordsFull";
 
+import {
+  mdiMenu,
+  mdiHome,
+  mdiMagnify,
+  mdiBookEditOutline,
+  mdiSortAlphabeticalVariant,
+  mdiTagMultiple,
+  mdiPackage,
+  mdiGithub,
+  mdiInstagram
+} from "@mdi/js";
+
 export default {
   components: { LanguageInput, SearchWordsFull },
   data() {
     return {
-      show: false
-      // menus: [
-      //   // { name: "Home", url: "/" },
-      //   // { name: "Words", url: "/word" },
-      //   // { name: "Topics", url: "/topic" }
-      //   // { name: "About", url: "/about" },
-      // ]
+      mdiMenu,
+      mdiHome,
+      mdiMagnify,
+      mdiGithub,
+      mdiInstagram,
+      toggleMenu: false,
+      toggleSearch: false,
+      menus: [
+        {
+          icon: mdiHome,
+          text: `Home`,
+          link: `/`,
+          color: `yellow darken-3`,
+          bgcolor: `tw-bg-yellow-50`
+        },
+        {
+          icon: mdiBookEditOutline,
+          text: `Dictionary`,
+          link: `/dictionary`,
+          color: `blue`,
+          bgcolor: `tw-bg-blue-50`
+        },
+        {
+          icon: mdiSortAlphabeticalVariant,
+          text: `Browse all words`,
+          link: `/browse`,
+          color: `pink`,
+          bgcolor: `tw-bg-pink-50`
+        },
+
+        {
+          icon: mdiPackage,
+          text: `Topics`,
+          link: `/topic`,
+          color: `green`,
+          bgcolor: `tw-bg-green-50`
+        },
+        {
+          icon: mdiTagMultiple,
+          text: `Labels`,
+          link: `/label`,
+          color: `indigo`,
+          bgcolor: `tw-bg-blue-50`
+        }
+      ]
     };
   }
-  // methods: {
-  //   hideMenu() {
-  //     this.show = false;
-  //   }
-  // }
 };
 </script>
 
 <style lang="sass" scoped>
-.header-custom
-  background: rgb(236,255,249)
-  background: linear-gradient(110deg, rgba(236,255,249,1) 0%, rgba(229,255,250,1) 50%, rgba(228,250,255,1) 100%)
-.ul-menu .nuxt-link-exact-active
-  text-shadow: 0.5px 0.5px 1px pink
-  @apply tw-text-secondary-focus tw-bg-white tw-shadow
-.header-nav-link
-  @apply tw-rounded-lg tw-p-1 md:tw-p-2.5 hover:tw-bg-white hover:tw-text-secondary-focus tw-font-medium
+// .header-custom
+//   background: rgb(236,255,249)
+//   background: linear-gradient(110deg, rgba(236,255,249,1) 0%, rgba(229,255,250,1) 50%, rgba(228,250,255,1) 100%)
+.nav .nuxt-link-exact-active
+  @apply tw-font-bold tw-bg-gray-100
 </style>
